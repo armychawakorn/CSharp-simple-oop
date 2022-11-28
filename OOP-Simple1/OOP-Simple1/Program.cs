628e92c76f0047e976f7a138d370b55fd0c0444b
RosinanteDev <armychawakorn@gmail.com>
2022-11-28 15:37:13 +0700

    Fix Name, studentID, Email

--- OOP-Simple1/OOP-Simple1/Program.cs
+++ OOP-Simple1/OOP-Simple1/Program.cs
@@ -6,7 +6,9 @@ namespace OOP_Simple1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("นายชวกร เนืองภา");
+            Console.WriteLine("ชื่อ: นายชวกร เนืองภา");
+            Console.WriteLine("รหัสนักศึกษา: 653450087-5");
+            Console.WriteLine("อีเมล: Chawakorn.n@kkumail.com");
             Console.ReadKey();
         }
     }
