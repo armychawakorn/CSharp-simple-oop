c87a3c721b171eb9e4d960cc2db6fdb05de542ac
RosinanteDev <armychawakorn@gmail.com>
2022-11-28 15:31:28 +0700

    Add write name method

---
+++ OOP-Simple1/OOP-Simple1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OOP_Simple1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("นายชวกร เนืองภา");
+            Console.ReadKey();
+        }
+    }
+}
